c53316e3764a0967256fa583141e41af26bb90a5
Hamzaabro5 <hamzaabro683@gmail.com>
2024-02-29 20:25:02 +0500

    Assignment

---
+++ app.js
@@ -0,0 +1,87 @@
+
+
+// // Question 1
+
+// var num = +prompt("Check Division of 3")
+
+// if(num % 3 ===0){
+//     console.log("Division Possible")
+// }else{
+//     console.log("Number is not divisible by 3")
+// }
+
+
+
+
+
+
+
+// // Question 2
+
+// var num = +prompt("Enter The Number");
+// if(num % 2 ===0){
+//     console.log("This is even number");
+// }
+// else if(num % 3 ===0){
+//     console.log("This is odd number");
+// }else if(num = 5){
+//     console.log("this is odd number");
+// }else if(num = 7){
+//     console.log("this is odd number");
+// }
+// else{
+//     console.log("Number Type not found");
+// }
+
+
+
+
+
+
+
+
+
+// // Question 3
+
+// var num = +prompt("Enter The Value");
+// if(num >= 18){
+//     console.log("Old Enough");
+// }else{
+//     console.log("Too Young");
+// }
+
+
+
+
+
+
+// // Question 4
+
+// var username = "hamza"
+// prompt("Enter Your Name");
+// if(username){
+//     console.log(document.write("Hey Hamza Welcome To Our Website"));
+// }else{
+//     console.log("Welcome To my website");
+// }
+
+
+
+
+
+
+
+
+
+
+
+// // Question 5
+
+// var num = +prompt("Check Division of 3")
+
+// if(num % 3 ===0){
+//     console.log("Division Possible")
+// }else{
+//     console.log("Number is not divisible by 3")
+// }
+
