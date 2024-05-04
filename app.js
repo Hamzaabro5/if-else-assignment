2e5f728e1cd25ccd41ce0a75812946963704edde
Hamzaabro5 <hamzaabro683@gmail.com>
2024-05-04 16:54:24 +0500

    if else Assignment

--- app.js
+++ app.js
@@ -1,37 +1,41 @@
 
 
-// // Question 1
+// // Answer No  1
 
 // var num = +prompt("Check Division of 3")
 
 // if(num % 3 ===0){
-//     console.log("Division Possible")
-// }else{
-//     console.log("Number is not divisible by 3")
-// }
+    //     console.log("Division Possible")
+    // }else{
+        //     console.log("Number is not divisible by 3")
+        // }
+        
+        
+// // Answer No  1
 
 
 
 
 
-
-
-// // Question 2
+// // Answer No 2
 
 // var num = +prompt("Enter The Number");
 // if(num % 2 ===0){
-//     console.log("This is even number");
-// }
-// else if(num % 3 ===0){
-//     console.log("This is odd number");
-// }else if(num = 5){
-//     console.log("this is odd number");
-// }else if(num = 7){
-//     console.log("this is odd number");
-// }
-// else{
-//     console.log("Number Type not found");
-// }
+    //     console.log("This is even number");
+    // }
+    // else if(num % 3 ===0){
+        //     console.log("This is odd number");
+        // }else if(num = 5){
+            //     console.log("this is odd number");
+            // }else if(num = 7){
+                //     console.log("this is odd number");
+                // }
+                // else{
+                    //     console.log("Number Type not found");
+                    // }
+                    
+ // // Answer No 2
+                    
 
 
 
@@ -39,31 +43,33 @@
 
 
 
-
-
-// // Question 3
+// // Answer No 3
 
 // var num = +prompt("Enter The Value");
 // if(num >= 18){
-//     console.log("Old Enough");
-// }else{
-//     console.log("Too Young");
-// }
+    //     console.log("Old Enough");
+    // }else{
+        //     console.log("Too Young");
+        // }
+        
+// // Answer No 3
 
 
 
 
 
-
-// // Question 4
+// // Answer No 4
 
 // var username = "hamza"
 // prompt("Enter Your Name");
 // if(username){
-//     console.log(document.write("Hey Hamza Welcome To Our Website"));
-// }else{
-//     console.log("Welcome To my website");
-// }
+    //     console.log(document.write("Hey Hamza Welcome To Our Website"));
+    // }else{
+        //     console.log("Welcome To my website");
+        // }
+        
+        
+ // // Answer No 4
 
 
 
@@ -73,15 +79,26 @@
 
 
 
-
-
-// // Question 5
+// // Answer No 5
 
 // var num = +prompt("Check Division of 3")
 
 // if(num % 3 ===0){
-//     console.log("Division Possible")
-// }else{
-//     console.log("Number is not divisible by 3")
-// }
+    //     console.log("Division Possible")
+    // }else{
+        //     console.log("Number is not divisible by 3")
+        // }
+        
+// // Answer No 5
 
+
+
+
+
+
+
+// Answer No 6
+
+
+
+// Answer No 6
