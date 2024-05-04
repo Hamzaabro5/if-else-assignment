c51892c8bc3361547979dccfe522f8739f1b19a7
Hamzaabro5 <hamzaabro683@gmail.com>
2024-05-04 17:56:38 +0500

    if else assignment

--- app.js
+++ app.js
@@ -300,3 +300,84 @@
 // }
 
 // // Answer No 15
+
+
+
+
+
+
+
+
+
+// // Answer No 16
+
+// var val = prompt();
+
+// if (val >= 0) {
+//     console.log(`Its number`);
+// }else if (val == "") {
+//     console.log(`Its string`);
+// }else if (val == true || val == false) {
+//     console.log(`Its Boolean`);
+// }
+
+// // Answer No 16
+
+
+
+
+
+
+
+
+
+// // Answer No 17
+
+
+// if (10 != 8 ) {
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+
+// // Answer No 17
+
+
+
+
+
+
+
+
+
+// // Answer No 18
+
+// var month = 8;
+
+// if (month == 1) {
+//     console.log(`January`);
+// }else if (month == 2) {
+//     console.log(`February`);
+// }else if (month == 3) {
+//     console.log(`March`);
+// }else if (month == 4) {
+//     console.log(`April`);
+// }else if (month == 5) {
+//     console.log(`May`);
+// }else if (month == 6) {
+//     console.log(`June`);
+// }else if (month == 7) {
+//     console.log(`July`);
+// }else if (month == 8) {
+//     console.log(`August`);
+// }else if (month == 9) {
+//     console.log(`September`);
+// }else if (month == 10) {
+//     console.log(`octuber`);
+// }else if (month == 11) {
+//     console.log(`November`);
+// }else if (month == 12) {
+//     console.log(`December`);
+// }
+
+// // Answer No 18
