dc0eff947d8deed36402e56ab4bf0332a92c8c2c
Hamzaabro5 <hamzaabro683@gmail.com>
2024-05-04 17:25:04 +0500

    if else assignment

--- app.js
+++ app.js
@@ -97,8 +97,97 @@
 
 
 
-// Answer No 6
+// // Answer No 6
+
+// var input = prompt(`Write Down any character(Number or String)?`);
+
+// if (input == input.toUpperCase()) {
+//     console.log(`Capital Letter h`);
+// }else{
+//     console.log(`Small Letter h`);
+// }
+
+
+// // Answer No 6
 
 
 
-// Answer No 6
+
+
+
+
+
+
+// // Answer No 7
+
+// var input = +prompt(`Enter Your time in 24 hour format?`);
+
+// if (input > 0o00 && input < 1200) {
+//     console.log(`Good Morning!`);
+// }else if (input >= 1200 && input < 1700)  {
+//     console.log(`Good Afternoon!`);
+// }else if (input >= 1700 && input < 2100)  {
+//     console.log(`Good Evening!`);
+// }else if (input >= 2100 && input < 2359)  {
+//     console.log(`Good night!`);
+// }else{
+//     console.log(`Please Enter Corrent Time.`);
+// }
+
+// // Answer No 7
+
+
+
+
+
+
+
+
+
+// // Answer No 8
+
+// var input = prompt(`Write down Year in YYYY Format to check if its leap year or not`);
+
+// if (input == 2000) {
+//     console.log(`Its Leap Year`);
+// }else if (input == 2004) {
+//     console.log(`Its Leap Year`);
+// }else if (input == 2008) {
+//     console.log(`Its Leap Year`);
+// }else if (input == 2012) {
+//     console.log(`Its Leap Year`);
+// }else if (input == 2016) {
+//     console.log(`Its Leap Year`);
+// }else if (input == 2020) {
+//     console.log(`Its Leap Year`);
+// }else if (input == 2024) {
+//     console.log(`Its Leap Year`);
+// }else if (input == 2028) {
+//     console.log(`Its Leap Year`);
+// }else if (input == 2032) {
+//     console.log(`Its Leap Year`);
+// }else if (input == 2036) {
+//     console.log(`Its Leap Year`);
+// }else if (input == 2040) {
+//     console.log(`Its Leap Year`);
+// }else if (input == 2044) {
+//     console.log(`Its Leap Year`);
+// }else if (input == 2048) {
+//     console.log(`Its Leap Year`);
+// }else if (input == 2052) {
+//     console.log(`Its Leap Year`);
+// }else if (input == 2056) {
+//     console.log(`Its Leap Year`);
+// }else if (input == 2060) {
+//     console.log(`Its Leap Year`);
+// }else if (input == 2062) {
+//     console.log(`Its Leap Year`);
+// }else if (input == 2066) {
+//     console.log(`Its Leap Year`);
+// }else if (input == 2070) {
+//     console.log(`Its Leap Year`);
+// }else{
+//     console.log(`Its not a leap year`);
+// }
+
+// // Answer No 8
